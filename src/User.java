904495ae0a09df438f2ee137f6a47666ff21d5c0
Andrew M <marders.andrew@protonmail.com>
2026-09-10 22:06:31 +0200

    This is a new branch that shows versioning. I have added a couple variables as well as a class constructor for the user class.

--- src/User.java
+++ src/User.java
@@ -1,7 +1,17 @@
+import java.util.ArrayList;
+
 /**
  * User Class that holds basic info about the user
  */
 
 public class User {
+    private String name; // User identifying information.
+    private ArrayList<Debt> listOfDebts = new ArrayList<>(); // An arraylist of debt objects used to represent all of the debts a user has. 
+    private ArrayList<Investment> listOfInvestments = new ArrayList<>(); // An arraylist of investment objects used to represent all of the investments a user has.
+    private Budget myBudget; // A budget item representing this user's current budget.
 
+
+    User(){
+
+    }
 }
